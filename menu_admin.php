f8862a2e5bba58f134ce8c14761b0612e50c6e5b
alexrp202 <72034440+alexrp202@users.noreply.github.com>
2020-12-10 10:55:01 +0100

    menu admin mejor y gestion productos en carpeta

--- menu_admin.php
+++ menu_admin.php
@@ -20,13 +20,14 @@ session_start();
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/bootstrap-theme.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	
+  
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <!-- /Bootstrap --> 
 
 </head>
-<body background="Fondo2.png" style="background-repeat: no-repeat; background-attachment: fixed; background-size: cover;">
+<body background="imagenes/fondo.jpg" style="background-repeat: no-repeat; background-attachment: fixed; background-size: cover;">
 <div class="container-fluid" height="100%">
   <div class="row" >
       <div class="col-3">
@@ -37,36 +38,53 @@ session_start();
 	<br>
 
 
-	<table  class="table table-hover">
-	<tr>
-		<td align="center"><button style='width:25%;' width="20%" type="button" onclick="location.href='a_index.php'" class="btn btn-primary btn-lg">Gestion de productos</button></td>
-	</tr>
-
-	<tr>
-		<td align="center"><button style='width:25%;' width="20%" type="button" onclick="location.href='usuarios/u_index.php'" class="btn btn-primary btn-lg">Gestion de usuarios</button></td>
-	</tr>
-
-
-
-
- 	</tr>		
-	<tr>
-		<td align="center"><button style='width:25%;' width="20%" type="button" onclick="location.href='mostrarjuegos.php'" class="btn btn-primary btn-lg">Ver productos</button></td>
-	 </tr>
-	 <tr>
-		<td align="center"><button style='width:25%;' width="20%" type="button" onclick="location.href='./carrito/index.php'" class="btn btn-primary btn-lg">Ver carrito</button></td>
- 	</tr>
-	<tr>
-		<td align="center"><a href="logout.php" onclick="javascript:return confirm('Estas seguro que quieres desloguearte??')"><button style='width:25%;' type="button"  onclick="location.href='logout.php'" class="btn btn-secondary btn-lg">Salir</button></a></td>
-</tr>
-
-	</table>
-
-			</div>
-		<div class="col-3" >
-
-			<!--<iframe frameborder="1" height="100%" width="100%" src="consultarRanking.php" name="ranking"></iframe>-->
 		</div>
+
+		<section id="portfolio" style="padding: 50px;">
+		<a href="productos/a_index.php">
+      <div class="row" >
+          <div class="col-md-6">
+              <div class="portfolio-container">
+                  
+                  <img src="imagenes/productos.jpg" class="img-fluid" alt="Portfolio 01">
+			  </div>
+			  </a>
+		  </div>
+		  <a href="usuarios/u_index.php">
+		  <div class="col-md-6">
+              <div class="portfolio-container">
+                 
+                  <img src="imagenes/usuarios.png" class="img-fluid" alt="Portfolio 01">
+			  </div>
+			  </a>
+		  </div>
+		  <a href="./carrito/index.php">
+		  <div class="col-md-6">
+              <div class="portfolio-container">
+                
+                  <img src="imagenes/carrito.jpg" class="img-fluid" alt="Portfolio 01">
+			  </div>
+			  </a>
+		  </div>
+		  <a onclick="javascript:return confirm('Estas seguro que quieres desloguearte??')" href="logout.php">
+		  <div class="col-md-6">
+              <div class="portfolio-container">
+                
+                  <img src="imagenes/logout.jpg" class="img-fluid" alt="Portfolio 01">
+			  </div>
+			  </a>
+          </div>
+      </div>
+
+
+</section>
+
+
+
+
+
+
+
 	</div>
 </div>
 
